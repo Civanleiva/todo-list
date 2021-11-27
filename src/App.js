57290bb4372d18e2663d9831b7ff06f053fc170e
Carlos <civanleiva42@gmail.com>
2021-11-27 17:01:44 -0600

    Added a new todo to the list

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import TodoList from "./components/TodoList";
 function App() {
   const todoList = [
     {content: 'Bathe the dog', completed: true, id: 1},
-    {content: 'Cook dinner', completed: false, id: 2}
+    {content: 'Cook dinner', completed: false, id: 2},
+    {content: 'Show how to use git', completed: false, id: 3}
   ];
+
+  // Change
 
   return (
     <div className="App">
